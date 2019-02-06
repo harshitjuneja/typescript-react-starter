238610215f9130fe9a478914ff4916bf909a9a36
Harshit Juneja <sayhitoharshit@gmail.com>
2019-02-07 00:52:52 +0530

    add react and react dom package and a sample application

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('My Minimal React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Getting things started with react babel webpack';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
